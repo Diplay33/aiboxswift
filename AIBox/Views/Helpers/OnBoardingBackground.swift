b8e83b63e629a7961861aa868e0be10e5bdc05e6
Jacques HU <jacqueshu@Apple-MacBook-Pro-14-M4-de-Diplay.local>
2024-11-19 14:18:27 +0100

    ui(onboarding): added background gradient

---
+++ AIBox/Views/Helpers/OnBoardingBackground.swift
@@ -0,0 +1,28 @@
+//
+//  OnBoardingBackground.swift
+//  AIBox
+//
+//  Created by Jacques HU on 19/11/2024.
+//
+
+import SwiftUI
+
+struct OnBoardingBackground<Content: View>: View {
+    var contentView: () -> Content
+    
+    init(@ViewBuilder contentView: @escaping () -> Content) {
+        self.contentView = contentView
+    }
+    
+    var body: some View {
+        contentView()
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(LinearGradient(gradient: Gradient(colors: [.white, .accentColor]), startPoint: .init(x: 0.79, y: 0.09), endPoint: .init(x: -3, y: 1.5)))
+    }
+}
+
+#Preview {
+    OnBoardingBackground {
+        Text("Hello, World!")
+    }
+}
---
+++ AIBox/Views/LoginView/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  AIBox
+//
+//  Created by Jacques HU on 19/11/2024.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        OnBoardingBackground {
+            Text("Hello World")
+        }
+    }
+}
+
+#Preview {
+    LoginView()
+}
