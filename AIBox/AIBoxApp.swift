e935c1c0ea9a221bc1a4f78821b5c8e3b8e9a911
Jacques HU <jacqueshu@Apple-MacBook-Pro-14-M4-de-Diplay.local>
2024-11-19 12:21:51 +0100

    (project): init xcode project

---
+++ AIBox/AIBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIBoxApp.swift
+//  AIBox
+//
+//  Created by Jacques HU on 19/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct AIBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIBoxTests/AIBoxTests.swift
@@ -0,0 +1,16 @@
+//
+//  AIBoxTests.swift
+//  AIBoxTests
+//
+//  Created by Jacques HU on 19/11/2024.
+//
+
+import Testing
+
+struct AIBoxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
